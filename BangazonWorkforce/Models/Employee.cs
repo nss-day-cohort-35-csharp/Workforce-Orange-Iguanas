55be1122a3c27ccf342e54469a8d17bf1af62bc2
stephensenft21 <stephensenft21@gmail.com>
2020-01-15 11:20:42 -0600

    built employee model

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -7,5 +7,15 @@ namespace BangazonWorkforce.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public string Email { get; set; }
+        public bool isSupervisor { get; set; }
+        public int ComputerId { get; set; }
+
+
+
     }
 }
