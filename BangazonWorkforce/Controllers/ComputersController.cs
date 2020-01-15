824439da21281c62537de2c75b32efd797a53a75
Shirish Shrestha <shrestha_sl@hotmail.com>
2020-01-15 13:40:11 -0600

    search query for computer list

--- BangazonWorkforce/Controllers/ComputersController.cs
+++ BangazonWorkforce/Controllers/ComputersController.cs
@@ -26,7 +26,7 @@ namespace BangazonWorkforce.Controllers
             }
         }
         // GET: Computers
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
             using (SqlConnection conn = Connection)
             {
@@ -34,6 +34,11 @@ namespace BangazonWorkforce.Controllers
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"SELECT Id, PurchaseDate, Make, Model FROM Computer ";
+                    if (!string.IsNullOrWhiteSpace(searchString))
+                    {
+                        cmd.CommandText += @" WHERE Make LIKE @searchString OR Model LIKE @searchString";
+                    }
+                    cmd.Parameters.Add(new SqlParameter("@searchString", "%" + searchString + "%"));
                     var reader = cmd.ExecuteReader();
                     var computers = new List<Computer>();
                     while (reader.Read())
@@ -43,8 +48,8 @@ namespace BangazonWorkforce.Controllers
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
                             Make = reader.GetString(reader.GetOrdinal("Make")),
-                            Model = reader.GetString(reader.GetOrdinal("Model")),
-                           
+                            Model = reader.GetString(reader.GetOrdinal("Model"))
+
                         });
                     }
                     reader.Close();
@@ -53,6 +58,106 @@ namespace BangazonWorkforce.Controllers
             }
         }
 
-       
+        // GET: Computer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Computer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Computer computer)
+        {
+            {
+                try
+                {
+                    using (SqlConnection conn = Connection)
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = @"INSERT INTO Computer (PurchaseDate, Make, Model)
+                                            VALUES (@PurchaseDate, @Make, @Model)";
+
+                            cmd.Parameters.Add(new SqlParameter("@PurchaseDate", computer.PurchaseDate));
+                            cmd.Parameters.Add(new SqlParameter("@Make", computer.Make));
+                            cmd.Parameters.Add(new SqlParameter("@Model", computer.Model));
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    return View();
+                }
+            }
+        }
+
+        // GET: Computer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"SELECT Id, PurchaseDate, Make, Model FROM Computer
+                                        WHERE Id = @Id";
+
+                        cmd.Parameters.Add(new SqlParameter("@Id", id));
+
+                        var reader = cmd.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            var computer = new Computer
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                Make = reader.GetString(reader.GetOrdinal("Make")),
+                                Model = reader.GetString(reader.GetOrdinal("Model"))
+
+                            };
+
+                            reader.Close();
+                            return View(computer);
+                        }
+                        return NotFound();
+                    }
+                }
+            }
+        }
+
+        // POST: Computer/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Computer computer)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Computer WHERE Id = @id";
+
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
