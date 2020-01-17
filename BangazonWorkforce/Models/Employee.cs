c5dadae6922b105ac0f5a7677a99338d610c9059
Shirish Shrestha <shrestha_sl@hotmail.com>
2020-01-16 20:49:47 -0600

    computer detail page added

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -9,11 +9,15 @@ namespace BangazonWorkforce.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+        [Display(Name="Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public string Email { get; set; }
         public bool IsSupervisor { get; set; }
+        [Display(Name="Computer")]
         public int ComputerId { get; set; }
         public Computer Computer { get; set; }
         public Department Department { get; set; }
