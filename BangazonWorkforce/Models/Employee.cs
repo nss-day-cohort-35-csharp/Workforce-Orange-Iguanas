42aa14d6e963d4c05cc042ae95eaa13836dea550
stephensenft21 <stephensenft21@gmail.com>
2020-01-17 09:54:25 -0600

    changes made to employee model

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -9,6 +9,7 @@ namespace BangazonWorkforce.Models
     public class Employee
     {
         public int Id { get; set; }
+
         [MinLength(1, ErrorMessage = "Please enter a First name."), MaxLength(20, ErrorMessage = "Characters exceeds 40 characters")]
 
         [Display(Name = "First Name")]
@@ -28,13 +29,15 @@ namespace BangazonWorkforce.Models
         [Display(Name = "Supervisor")]
 
         public bool IsSupervisor { get; set; }
+
         [Required]
         [Display(Name = "Computer")]
+
 
         public int ComputerId { get; set; }
         public Computer Computer { get; set; }
         public Department Department { get; set; }
-            public List <TrainingProgram> trainingPrograms { get; set; }
-        public TrainingProgram TrainingProgram { get; set; } 
+        public List<TrainingProgram> trainingPrograms { get; set; }
+        public TrainingProgram TrainingProgram { get; set; }
     }
 }
